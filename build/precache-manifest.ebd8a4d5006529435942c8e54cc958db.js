3bc204ef8d5097151fa422b64891a009c0d1f6bb
marc <matherrien@hotmail.ca>
2020-04-09 11:14:08 -0400

    base project

---
+++ build/precache-manifest.ebd8a4d5006529435942c8e54cc958db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d839ac130b94edb9c8c88fea6e26fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb6e3df454795c9ee930",
+    "url": "/static/js/2.c79e89db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c79e89db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acd413c129abee8578a6",
+    "url": "/static/js/main.a11d2551.chunk.js"
+  },
+  {
+    "revision": "0058ea9837278a0395d9",
+    "url": "/static/js/runtime-main.9d11840c.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/build/index.html'));
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port);
+
+
+console.log('App is listening on port ' + port);
